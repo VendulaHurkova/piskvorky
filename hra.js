ceeb8b3193f56415b006d41262cec74053b7e622
Vendula Hůrková <hurkova.vendula@gmail.com>
2024-04-18 21:46:32 +0200

    úkol 4

--- hra.js
+++ hra.js
@@ -1,54 +1,88 @@
-const buttonsIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let currentPlayer = "circle";
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
+const gameBoard = [];
+const prepareGameBoard = () => {
+  for (let i = 0; i < 100; i++) {
+    gameBoard.push('_');
+  }
+};
+
+let currentPlayer = 'circle';
 
 const showCircle = () => {
-  const imgCross = document.querySelector("#imgCross");
-  imgCross.classList.remove("visible");
-  imgCross.classList.add("invisible");
-  const imgCircle = document.querySelector("#imgCircle");
-  imgCircle.classList.remove("invisible");
-  imgCircle.classList.add("visible");
+  const imgCross = document.querySelector('#imgCross');
+  imgCross.classList.remove('visible');
+  imgCross.classList.add('invisible');
+  const imgCircle = document.querySelector('#imgCircle');
+  imgCircle.classList.remove('invisible');
+  imgCircle.classList.add('visible');
 };
 
 const showCross = () => {
-  const imgCircle = document.querySelector("#imgCircle");
-  imgCircle.classList.remove("visible");
-  imgCircle.classList.add("invisible");
-  const imgCross = document.querySelector("#imgCross");
-  imgCross.classList.remove("invisible");
-  imgCross.classList.add("visible");
+  const imgCircle = document.querySelector('#imgCircle');
+  imgCircle.classList.remove('visible');
+  imgCircle.classList.add('invisible');
+  const imgCross = document.querySelector('#imgCross');
+  imgCross.classList.remove('invisible');
+  imgCross.classList.add('visible');
+};
+
+const checkGameState = () => {
+  const result = findWinner(gameBoard);
+  if(result != null) {
+    let endGameNote = '';
+    if (result === 'x' || result == 'o') {
+      endGameNote = `Vyhrál hráč se symbolem ${result}.`
+    }
+    else if (result === 'tie') {
+      endGameNote = 'Hra skončila nerozhodně.'
+    }
+    setTimeout(function() {
+      alert(endGameNote);
+      location.reload();
+    }, 100);
+  }
 };
 
 const onButtonClick = (event) => {
   const button = event.target;
-  if (currentPlayer == "cross") {
-    button.classList.add("board__field--cross");
-    currentPlayer = "circle";
+  const buttonIndex = Number(button.id.substring('button'.length));
+  if (currentPlayer == 'cross') {
+    button.classList.add('board__field--cross');
+    gameBoard[buttonIndex] = 'x';
+    currentPlayer = 'circle';
     showCircle();
   } else {
-    button.classList.add("board__field--circle");
-    currentPlayer = "cross";
+    button.classList.add('board__field--circle');
+    gameBoard[buttonIndex] = 'o';
+    currentPlayer = 'cross';
     showCross();
   }
-  button.disabled = true
+  button.disabled = true;
+  checkGameState();
 };
 
 const setAkcionToButtons = () => {
-  buttonsIds.forEach((buttonId) => {
-    const button = document.querySelector("#button" + buttonId);
-    button.addEventListener("click", onButtonClick);
+  const table = document.querySelector('.play-table');
+  const allButtons = table.querySelectorAll('button');
+  allButtons.forEach((button) => {
+    button.addEventListener('click', onButtonClick);
   });
 };
 
-setAkcionToButtons();
-
+const initGame = () => {
+  setAkcionToButtons();
+  prepareGameBoard();
+};
 
 const showRestartDialog = (event) => {
-  const answer = confirm("Opravdu chcete začít znovu?");
+  const answer = confirm('Opravdu chcete začít znovu?');
   if (answer == false) {
-    event.preventDefault()
+    event.preventDefault();
   }
-}
+};
 
-const buttonRestart = document.querySelector("#buttonRestart");
-buttonRestart.addEventListener("click", showRestartDialog);
+const buttonRestart = document.querySelector('#buttonRestart');
+buttonRestart.addEventListener('click', showRestartDialog);
+
+initGame();
